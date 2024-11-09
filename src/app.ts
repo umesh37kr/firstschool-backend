40a3f28a7351610327258a888e2cb1e1e564cd9d
umesh <umesh37kr@gmail.com>
2024-11-09 12:04:43 +0530

    add global error handlers

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import express from "express";
+import globalErrorHandlers from "./middlewares/globalErrorHandles";
 const app = express();
 
 // routes
 app.get("/", (req, res) => {
   res.json({ message: "welcome to First School" });
 });
+
+// global error handlers
+app.use(globalErrorHandlers);
+
 export default app;
