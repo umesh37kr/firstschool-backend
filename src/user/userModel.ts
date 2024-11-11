91a7771f120604c3a35f4bea67d4681d4c80a534
umesh <umesh37kr@gmail.com>
2024-11-11 23:40:08 +0530

    add user model and user types

---
+++ src/user/userModel.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { User } from "./userType";
+
+const userSchema = new mongoose.Schema<User>(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model<User>("User", userSchema);
