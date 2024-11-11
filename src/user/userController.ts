67bb69c9e9cbfd9f64909f0bb6b4985002ce0a37
umesh <umesh37kr@gmail.com>
2024-11-12 00:13:25 +0530

    create user data

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,15 +1,30 @@
 import { NextFunction, Request, Response } from "express";
 import { validationResult } from "express-validator";
 import createHttpError from "http-errors";
-
-const createUser = (req: Request, res: Response, next: NextFunction) => {
+import userModel from "./userModel";
+import bcrypt from "bcrypt";
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const result = validationResult(req);
   if (!result.isEmpty()) {
     return next(createHttpError(400, result.array()[0].msg as string));
     // return res.status(400).json({ errors: result.array() });
   }
-  const userData = req.body;
-  res.json({ message: "user created", userData });
+  const { username, password, role } = req.body;
+  const user = await userModel.findOne({ username });
+  if (user) {
+    return next(createHttpError(409, "username is already exist"));
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+  // token generation
+
+  // res
+  const newUser = await userModel.create({
+    username,
+    role,
+    password: hashedPassword,
+  });
+
+  res.json({ id: newUser._id });
 };
 
 export { createUser };
