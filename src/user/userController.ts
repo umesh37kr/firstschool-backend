72dcdaab9cf8b5014ba7ee0c47beefe59b65543a
umesh <umesh37kr@gmail.com>
2024-11-12 22:59:05 +0530

    add error handling for user creation

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -5,6 +5,7 @@ import userModel from "./userModel";
 import bcrypt from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
+
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const result = validationResult(req);
   if (!result.isEmpty()) {
@@ -12,23 +13,28 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     // return res.status(400).json({ errors: result.array() });
   }
   const { username, password, role } = req.body;
-  const user = await userModel.findOne({ username });
-  if (user) {
-    return next(createHttpError(409, "username is already exist"));
+  try {
+    const user = await userModel.findOne({ username });
+    if (user) {
+      return next(createHttpError(409, "username is already exist"));
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // res
+    const newUser = await userModel.create({
+      username,
+      role,
+      password: hashedPassword,
+    });
+    // token generation
+    const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+      expiresIn: "7d",
+      algorithm: "HS256",
+    });
+    res.json({ accessToken: token });
+  } catch (error) {
+    console.log(error);
+    return next(createHttpError(500, "error while creating user"));
   }
-  const hashedPassword = await bcrypt.hash(password, 10);
-  // res
-  const newUser = await userModel.create({
-    username,
-    role,
-    password: hashedPassword,
-  });
-  // token generation
-  const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
-    expiresIn: "7d",
-    algorithm: "HS256",
-  });
-  res.json({ accessToken: token });
 };
 
 export { createUser };
