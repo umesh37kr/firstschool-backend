0be2473383dd63d1c8a39c32495c5855e35b697e
umesh <umesh37kr@gmail.com>
2024-12-06 16:55:50 +0530

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 import express from "express";
+import cors from "cors";
 import globalErrorHandlers from "./middlewares/globalErrorHandles";
 import UserRouter from "./user/userRouter";
 import studentRouter from "./student/studentRouter";
 import teacherRouter from "./teacher/teacherRouter";
+import { config } from "./config/config";
 const app = express();
+
+const corsOptions = {
+  origin: config.frontendDomain,
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 // routes
 app.get("/", (req, res) => {
