50256d9594025be369f5cfff33b41cd44e1babb3
umesh <umesh37kr@gmail.com>
2024-12-25 20:56:10 +0530

    add route to fetch post list

--- src/contactUs/contactController.ts
+++ src/contactUs/contactController.ts
@@ -26,3 +26,20 @@ export const createPost = async (
     console.log(error);
   }
 };
+
+export const postList = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const list = await contactModel.find();
+    if (!list) {
+      return res.status(404).json({ message: "Records not found.." });
+    }
+    return res.status(200).json({ data: list });
+  } catch (error) {
+    console.log("error:", error);
+    return next(createHttpError(500, "something went wrong.."));
+  }
+};
--- src/contactUs/contactRouter.ts
+++ src/contactUs/contactRouter.ts
@@ -1,8 +1,10 @@
 import express from "express";
-import { createPost } from "./contactController";
+import { createPost, postList } from "./contactController";
 import contactValidator from "./contact-validator";
+import authenticate from "../middlewares/authenticate";
 const contactRouter = express.Router();
 
 contactRouter.post("/create", contactValidator, createPost);
+contactRouter.get("/post-list", authenticate, postList);
 
 export default contactRouter;
